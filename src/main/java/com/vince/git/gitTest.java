ff009ccf1281bb7d8d76efe63c6d7f59077b52c2
yuehang <1764215438@qq.com>
2022-10-10 16:58:43 +0800

    git test 2

--- src/main/java/com/vince/git/gitTest.java
+++ src/main/java/com/vince/git/gitTest.java
@@ -3,6 +3,6 @@ package com.vince.git;
 public class gitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git 2!");
     }
 }
