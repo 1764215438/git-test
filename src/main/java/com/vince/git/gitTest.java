b20df58428488bc563ae1e145363abfe7e4cd980
yuehang <1764215438@qq.com>
2022-10-10 17:19:53 +0800

    hot-fix 2

--- src/main/java/com/vince/git/gitTest.java
+++ src/main/java/com/vince/git/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git 3!");
         System.out.println("hello hot-fix!");
+        System.out.println("hello hot-fix two!");
     }
 }
